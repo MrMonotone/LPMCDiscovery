9b0fed636f0dc52e101318fc1c9547c70d132b23
Mentioum <mentioum@gmail.com>
2011-07-16 03:04:18 +0100

    First Commit

---
+++ src/me/Mentioum/LPMCDiscovery/DiscoveryBlockListener.java
@@ -0,0 +1,25 @@
+package me.Mentioum.LPMCDiscovery;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+class DiscoveryBlockListener extends BlockListener {
+    public static LPMCDiscovery plugin;
+    public DiscoveryBlockListener(LPMCDiscovery instance){
+        plugin = instance;
+    }
+
+    public void onBlockPlace (BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlockPlaced();
+        
+        if (block.getType() == Material.DIRT && plugin.enabled(player)){
+            player.sendMessage("You dirty boy you!");
+        }
+        
+    }
+    
+}
---
+++ src/me/Mentioum/LPMCDiscovery/LPMCDiscovery.java
@@ -0,0 +1,90 @@
+package me.Mentioum.LPMCDiscovery;
+
+//Imports
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event.Type;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+
+public class LPMCDiscovery extends JavaPlugin {
+    public static LPMCDiscovery plugin;
+    public static final Logger logger = Logger.getLogger("Minecraft");
+    private DiscoveryBlockListener blocklistener = new DiscoveryBlockListener(this); //Defines Block Listener
+    public HashMap<Player, ArrayList<Block>> discoveryUsers = new HashMap(); //Defines HashMap
+    public HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>(); //Defines HashMap Debugees
+    public static String mainDirectory = "plugins/LPMCDiscovery";//Set main directory for easy reference
+    
+    
+    
+    @Override
+    public void onDisable() {
+        PluginManager pm = getServer().getPluginManager();
+        PluginDescriptionFile pdfFile = this.getDescription();
+        LPMCDiscovery.logger.info( pdfFile.getName() + " V." + pdfFile.getVersion() + " by " + pdfFile.getAuthors() + " is disabled");
+    }
+
+    @Override
+    public void onEnable() {
+    PluginManager pm = getServer().getPluginManager();
+    pm.registerEvent(Type.BLOCK_PLACE, this.blocklistener, Priority.Normal, this);
+    PluginDescriptionFile pdfFile = this.getDescription();
+    LPMCDiscovery.logger.info( pdfFile.getName() + " V" + pdfFile.getVersion() + " by " + pdfFile.getAuthors() + " is enabled");
+   
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        String[] split = args;
+        if (commandLabel.equalsIgnoreCase("discovery")
+                || commandLabel.equalsIgnoreCase("disc")){
+            toggleVision ((Player) sender);
+            return true;
+        }
+            return false;
+    }
+    
+    public void toggleVision(Player player){
+        if (enabled(player)) {
+            this.discoveryUsers.remove(player);
+            player.sendMessage("Discovery Disabled");
+        } else {
+            this.discoveryUsers.put(player, null);
+            player.sendMessage("Discovery Enabled");
+        }
+        
+    }   
+    
+    public boolean isDebugging(Player player){
+        if (debugees.containsKey(player)){
+            return debugees.get(player);
+        } else {
+            return false;
+        }
+        
+    }
+    
+    //Below checks to see if the player is in the HashMap
+    public void setDebugging (Player player, boolean value) {
+        debugees.put(player, value);
+    }
+    
+    public boolean enabled(Player player){
+        return this.discoveryUsers.containsKey(player);
+        }
+    
+
+}
